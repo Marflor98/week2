e752c868f3da8e388497cd52074ce01aadcfd034
Marflor98 <100226529+Marflor98@users.noreply.github.com>
2023-02-10 10:41:58 -0600

    Update compoundInterest.kt

--- compoundInterest.kt
+++ compoundInterest.kt
@@ -1,1 +1,8 @@
+import kotlin.math.*
 
+fun main(args: Array<String>) {
+    val base = 2.0
+    val exponent = 4
+    val result = Math.pow(base, exponent.toDouble())
+    println("$base raised to the power of $exponent is $result")
+}
