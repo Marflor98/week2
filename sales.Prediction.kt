a3a30cf615142350c162e79e2c87f961ba02fcb2
Marflor98 <100226529+Marflor98@users.noreply.github.com>
2023-02-14 08:56:01 -0600

    Update sales.Prediction.kt

--- sales.Prediction.kt
+++ sales.Prediction.kt
@@ -1,1 +1,9 @@
+import kotlin.math.*
 
+fun main(args: Array<String>) {
+    val base = 2
+    val exponent = 3
+    val result = Math.pow(base.toDouble(), exponent.toDouble())
+
+    println("Answer = $result")
+}
